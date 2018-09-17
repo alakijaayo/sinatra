70fece03add5a54e226dccf674d151e74ae73aa5
alakijayo <alakijaayo@gmail.com>
2018-09-17 16:46:52 +0100

    Keeping views clean

--- app.rb
+++ app.rb
@@ -14,7 +14,6 @@ get '/power' do
 end
 
 get '/kitty' do
-  "<div style='border: 3px dashed red'>
-    <img src = http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png />
-  </div>"
+  @names = ["Amigo", "Walter", "Batman"].sample
+  erb(:index)
 end
